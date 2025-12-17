e0fff59797c85abe2d0b5389ec30ac3892e4a882
Clara <cfanget@ensc.fr>
2025-12-17 18:19:16 +0900

    [UPDATE] - docker

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 const redisClient = require("./config/redis");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
   try {
